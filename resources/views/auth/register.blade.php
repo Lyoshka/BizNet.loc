e5b58179da3f360aa5272c56105d00655b5db595
Lyoshka <boygruv@gmail.com>
2016-04-18 21:18:03 +0300

    Admin Complete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,8 @@
  */?>
 @extends('layouts.master')
 @section('body')
-    {!! Form::open() !!}
+    <h2><p class="text-center">Страница регистрации</p></h2>
+    {!! Form::open(array('class'=> 'form-horizontal')) !!}
     @include('widgets.form._formitem_text', ['name' => 'email', 'title' => 'Email', 'placeholder' => 'Email' ])
     @include('widgets.form._formitem_password', ['name' => 'password', 'title' => 'Пароль', 'placeholder' => 'Пароль' ])
     @include('widgets.form._formitem_password', ['name' => 'password_confirm', 'title' => 'Подтверждение пароля', 'placeholder' => 'Пароль' ])
