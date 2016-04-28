1e849e3952d1cad749a94681917a7c8bc688cfbe
Lyoshka <boygruv@gmail.com>
2016-04-28 22:43:06 +0300

    End 28-04-16

--- resources/views/header/head.blade.php
+++ resources/views/header/head.blade.php
@@ -1,5 +1,7 @@
 {!! HTML::style('/style/css/bootstrap.min.css') !!}
 {!! HTML::style('/style/css/font-awesome.min.css') !!}
-{!! HTML::style('/style/css/style.css') !!}
+{!! HTML::style('/style/css/style.css') !!} 
+{!! HTML::script('/style/js/jquery-1.11.1.min.js') !!}
 {!! HTML::script('/style/js/bootstrap.min.js') !!}
+
 
--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -22,29 +22,25 @@
       </ul>
       <ul class="nav navbar-nav navbar-right">
 	  
-		<li class="dropdown">
+            @if(Sentinel::check())
+				<li class="active">
 			
-			    
-					  <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="fa fa-user fa-fw"></i>Action
-						<span class="caret"></span>
-					  </a>
-					  <ul class="dropdown-menu dropdown-user">
-						 <li><a href="/public/login">1</a></li>
-					  </ul>
-				
-		</li>
-
-
-        <li class="active"><a href="/public/login" class="btn_login btn btn-default navbar-btn">
-
-                    @if(Sentinel::check())
+					<a href="#" class="btn btn-default navbar-btn dropdown-toggle btn_login btn-group" data-toggle="dropdown">
                         <i class="fa fa-user fa-fw"></i> {{ Sentinel::check()->first_name  }} <span class="caret"></span>
-                    @else
-                        Вход
-                    @endif
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="/public/logout"><i class="fa fa-user fa-lg"> </i> Мой профиль</a></li>
+						<li><a href="/public/logout"><i class="fa fa-envelope-o fa-lg"> </i> Сообщения</a></li>
+						<li class="divider"></li>
+						<li><a href="/public/logout"><i class="fa fa-sign-out fa-lg"> </i> Выход</a></li>
+					</ul>
+					
+				</li>					
+            @else
+				<li class="active"><a href="/public/login" class="btn_login btn btn-default navbar-btn">Вход</a></li>
+            @endif
 
-		</a></li>
+			
 
 	    @if( Sentinel::guest() )
 	        <li><a href="/public/register" class="btn btn-default navbar-btn btn_reg">Регистрация</a></li>
